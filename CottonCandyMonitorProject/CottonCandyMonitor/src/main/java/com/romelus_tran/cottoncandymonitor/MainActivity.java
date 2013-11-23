ded2753c924def3d19b7ef7d0b799990ac575a7b
SnoBoarder <bttran89@gmail.com>
2013-11-23 16:48:07 -0500

    Added achartengine activity and displaying pseudo process list.

--- CottonCandyMonitorProject/CottonCandyMonitor/src/main/java/com/romelus_tran/cottoncandymonitor/MainActivity.java
+++ CottonCandyMonitorProject/CottonCandyMonitor/src/main/java/com/romelus_tran/cottoncandymonitor/MainActivity.java
@@ -1,33 +1,94 @@
 package com.romelus_tran.cottoncandymonitor;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.romelus_tran.cottoncandymonitor.graphs.LineGraph;
+import com.romelus_tran.cottoncandymonitor.monitor.CottonCandyMonitor;
+import com.romelus_tran.cottoncandymonitor.monitor.collectors.CPUCollector;
+
+import java.util.ArrayList;
+
+/**
+ * The primary activity that displays the graph button and the current processes running on the
+ * device. This activity extends ActionBarActivity, allowing for a menu panel to be displayed
+ * whenever the menu button is selected.
+ */
 public class MainActivity extends ActionBarActivity {
 
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> processesList;
+    TextView numProcesses;
+
+    /**
+     * Construct the MainActivity. This is where we get the
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // TODO: register metrics here:
+        // NOTE: CottonCandyMonitor has an error (need to import "StopWatch"), which is why this is
+        // commented out.
+        // CottonCandyMonitor.getInstance.register(new CPUCollector, <listener>);
+
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        // store the TextView that displays total processes running on the device
+        numProcesses = (TextView)findViewById(R.id.num_processes);
+
+        // initialize processesList
+        processesList = new ArrayList<String>();
+
+        // Create the adapter based on processesList. Now whenever processesList is updated,
+        // the adapter will notify the ListView to update.
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, processesList);
+
+        // Get the processesListView and set the arrayAdapter
+        ListView processesListView = (ListView)findViewById(R.id.list_processes);
+        processesListView.setAdapter(arrayAdapter);
+
+        // Register onClick event for every item in the list.
+        // NOTE: This is currently sending a "Toast" to the user at the moment.
+        // TODO: The listener should create an intent, which should be passed to the processorView
+        processesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedProcess = processesList.get(position);
+                Toast.makeText(getApplicationContext(), "Process Selected: " + selectedProcess,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    /**
+     * Whenever we return to MainActivity, onResume() will be called. Call refreshList()..
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshList();
+    }
+
+    /**
+     * Create the main menu.
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        // Inflate the menu. This adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -36,27 +97,47 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+
+            case R.id.refresh_list:
+                // refresh the list
+                refreshList();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * Update the processes list here and notify the adapter that the list has changed.
      */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+    private void refreshList() {
+        //TODO: Replace this with an actual fetch to the real process list. populate accordingly
+        processesList.add(getRandomProcess());
+        numProcesses.setText("Total Running Processes: " + processesList.size());
+        arrayAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * This starts a new activity displaying a demo line graph.
+     * TODO: Update the line graph to show real data and consider displaying the graph in MainActivity
+     * @param view
+     */
+    public void lineGraphHandler(View view) {
+        LineGraph line = new LineGraph();
+        Intent lineIntent = line.getIntent(this);
+        startActivity(lineIntent);
+    }
+
+    /**
+     * Test function to populate processes list.
+     * TODO: Remove this function when we get real data.
+     */
+    private String getRandomProcess() {
+        String[] s = new String[]{"Jet Pack Joyride", "Chrome", "Facebook", "Fruit Ninja", "Internet"};
+        return s[(int)Math.round(Math.random()* s.length)];
+    }
 }
--- CottonCandyMonitorProject/CottonCandyMonitor/src/main/java/com/romelus_tran/cottoncandymonitor/graphs/Linegraph.java
+++ CottonCandyMonitorProject/CottonCandyMonitor/src/main/java/com/romelus_tran/cottoncandymonitor/graphs/Linegraph.java
@@ -1,7 +1,64 @@
 package com.romelus_tran.cottoncandymonitor.graphs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
 /**
  * Created by Brian on 11/21/13.
+ * Create a LineGraph, in this case this will be the CPU display.
  */
-public class Linegraph {
+public class LineGraph {
+
+    /**
+     * Generates the intent for the line graph to pass into the graph activity.
+     * @param context refers to the current activity
+     * @return the entent
+     */
+    public Intent getIntent(Context context) {
+
+        // populate data with x/y positions
+        int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] y = { 30, 34, 45, 57, 77, 89, 5, 111, 123, 145 };
+
+        // generate series based on x/y positions stated above.
+        // A series simply requires x and y info.
+        // TODO: Dynamically update data. Review the following links:
+        // http://stackoverflow.com/questions/12948708/dynamically-updated-charts-with-achartengine
+        // https://code.google.com/p/achartengine/source/browse/trunk/achartengine/demo/org/achartengine/chartdemo/demo/chart/XYChartBuilder.java
+        TimeSeries series = new TimeSeries("Line1");
+        for (int i = 0; i < x.length; i++) {
+            series.add(x[i], y[i]);
+        }
+
+        // Create dataset and add series to dataset
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+        dataset.addSeries(series); // add line to dataset
+
+        // create renderer for the series
+        // Change renderer settings to tweak the line aesthetic
+        XYSeriesRenderer renderer = new XYSeriesRenderer();
+        renderer.setColor(Color.rgb(116,192,205)); // Set the color of the line to white
+        renderer.setLineWidth(5);
+        renderer.setPointStyle(PointStyle.POINT);
+
+        // create renderer and add renderer
+        // Change mRenderer settings to tweak the graph aesthetic
+        XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+        mRenderer.addSeriesRenderer(renderer);
+        mRenderer.setApplyBackgroundColor(true);
+        mRenderer.setBackgroundColor(Color.BLACK);
+        mRenderer.setChartTitle("CPU Usage");
+
+        Intent intent = ChartFactory.getLineChartIntent(context, dataset, mRenderer, "Line Graph Title");
+
+        return intent;
+    }
 }
